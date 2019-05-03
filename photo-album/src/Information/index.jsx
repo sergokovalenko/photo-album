3cd06ee564bfb5076811e243920045ecc6fa278d
Sergei Kovalenko <kovalenkosergey.98@yandex.ru>
2019-05-03 21:18:56 +0400

    Added information bloc. Currently sated for user information with very simple design

---
+++ photo-album/src/Information/index.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Button from './../components/Button';
+import './index.scss';
+
+class Information extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isBioShown: false
+        }
+    }
+
+    showBio = () => {
+        this.setState(state => ({ isBioShown: !state.isBioShown }));
+    }
+
+    render() {
+        const {
+            biografy = 'biografy',
+            name = 'Name Surname',
+            nickName = 'nickname',
+            photoCount = 5
+        } = this.props;
+        const { isBioShown } = this.state;
+        const onClick = () => console.log('click');
+
+        return (
+            <div className="wrapper">
+                <div className="info">
+                    <div className="row">
+                        <div className="col-6 picture">
+                            <img
+                                className="w-100 img-fluid img-thumbnail rounded-circle"
+                                src="https://im0-tub-ru.yandex.net/i?id=1ccc212c9173bbd571b5721dd8a0b1d1-l&n=13"
+                                alt="log"
+                                width="50%"
+                            />
+                        </div>
+                        <div className="col-6 information">
+                            <div className="user-info">
+                                <div className="user-info-name">{name}</div>
+                                <div className="user-info-nick ml-3">{nickName}</div>
+                                <div className="user-info-bio">
+                                    <span>About: </span>
+                                    {
+                                        isBioShown ?
+                                            <span>{biografy} </span> :
+                                            null
+                                    }
+                                    <span className="user-info-bio--link" onClick={() => this.showBio()}>
+                                        {isBioShown ? 'Hide...' : 'Show...'}
+                                    </span>
+                                </div>
+                                <div className="user-info-photos">{photoCount} photos</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="buttons-container">
+                    <Button content="Add" onClick={onClick} />
+                    <Button content="Change" onClick={onClick} />
+                    <Button content="Create" onClick={onClick} />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Information;
--- photo-album/src/index.js
+++ photo-album/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './DefaultApp/App';
+import InformationBlock from './Information';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<InformationBlock />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
